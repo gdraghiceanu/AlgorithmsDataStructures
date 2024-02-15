c9db1bd6c6c4eb3ae689af85cde0365138193b0d
George Draghiceanu <george.draghiceanu@gmail.com>
2024-02-15 12:42:24 +0200

    First Commit

---
+++ AlgorithmsDataStructuresProblems/AlgorithmsDataStructuresProblems/Program.cs
@@ -0,0 +1,36 @@
+﻿using AlgorithmsDataStructuresProblems.Interfaces;
+using AlgorithmsDataStructuresProblems.Warmup;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AlgorithmsDataStructuresProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salut");
+
+            var problems = new Dictionary<int, IProblem>()
+            {
+                { 1, new SumArrays() }
+            };
+
+            Console.WriteLine("Selectează numărul problemei de rezolvat:");
+            foreach (var pb in problems)
+            {
+                var nume = pb.Value.ToString();
+                var numeFinal = nume.Substring(nume.LastIndexOf(".") + 1);
+                Console.WriteLine($"Problema {pb.Key} - {numeFinal}");
+            }
+
+            if (int.TryParse(Console.ReadLine(), out int problemNumber) && problems.ContainsKey(problemNumber))
+            {
+                problems[problemNumber].Solve();
+            }
+            else
+            {
+                Console.WriteLine("Problemă invalidă sau număr greșit.");
+            }
+        }
+    }
+}
---
+++ AlgorithmsDataStructuresProblems/AlgorithmsDataStructuresProblems/Warmup/SumArrays.cs
@@ -0,0 +1,31 @@
+﻿using AlgorithmsDataStructuresProblems.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructuresProblems.Warmup
+{
+    public class SumArrays : IProblem
+    {
+        public void Solve()
+        {
+            Console.WriteLine("Rezolvarea problemei SumArrays");
+            var res = simpleArraySum(new List<int> { 1, 2, 3 });
+
+            Console.WriteLine(res);
+        }
+
+        public int simpleArraySum(List<int> ar)
+        {
+            var sum = 0;
+            foreach (var item in ar)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+    }
+}
